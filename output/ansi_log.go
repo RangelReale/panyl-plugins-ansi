c7d9534b2cd0fd547c3f37a1b75e2c8d00fb2905
Rangel Reale <rangelspam@gmail.com>
2022-04-05 09:49:32 -0300

    Initial import

---
+++ output/ansi_log.go
@@ -0,0 +1,58 @@
+package output
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/RangelReale/panyl"
+	"github.com/RangelReale/panyl/util"
+	"github.com/fatih/color"
+)
+
+type AnsiLog struct {
+	ShowSource bool
+}
+
+func (l AnsiLog) LogSourceLine(n int, line, rawLine string) {
+	red := color.New(color.FgRed)
+
+	red.Printf("@@@ SOURCE LINE [%d]: '%s' @@@\n", n, line)
+}
+
+func (l AnsiLog) LogProcess(p *panyl.Process) {
+	green := color.New(color.FgGreen)
+
+	var lineno string
+	if p.LineCount > 1 {
+		lineno = fmt.Sprintf("[%d-%d]", p.LineNo, p.LineNo+p.LineCount-1)
+	} else {
+		lineno = fmt.Sprintf("[%d]", p.LineNo)
+	}
+
+	var buf bytes.Buffer
+
+	if len(p.Metadata) > 0 {
+		_, _ = buf.WriteString(fmt.Sprintf("Metadata: %+v", p.Metadata))
+	}
+	if len(p.Data) > 0 {
+		if buf.Len() > 0 {
+			_, _ = buf.WriteString(" - ")
+		}
+		_, _ = buf.WriteString(fmt.Sprintf("Data: %+v", p.Data))
+	}
+
+	if len(p.Line) > 0 {
+		if buf.Len() > 0 {
+			_, _ = buf.WriteString(" - ")
+		}
+		_, _ = buf.WriteString(fmt.Sprintf("Line: \"%s\"", p.Line))
+	}
+
+	if l.ShowSource && len(p.Source) > 0 {
+		if buf.Len() > 0 {
+			_, _ = buf.WriteString(" - ")
+		}
+		_, _ = buf.WriteString(fmt.Sprintf("Source: \"%s\"", util.DoAnsiEscapeString(p.Source)))
+	}
+
+	green.Printf("*** PROCESS LINE %s: %s\n", lineno, buf.String())
+}
---
+++ output/ansi_output.go
@@ -0,0 +1,96 @@
+package output
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/RangelReale/panyl"
+	"github.com/fatih/color"
+	"time"
+)
+
+type AnsiOutputSprintfFunc func(format string, a ...interface{}) string
+
+type AnsiOutput struct {
+	ColorInformation, ColorWarning, ColorError, ColorInternalError, ColorUnknown AnsiOutputSprintfFunc
+}
+
+func NewAnsiOutput(ansi bool) *AnsiOutput {
+	ret := &AnsiOutput{
+		ColorError:         fmt.Sprintf,
+		ColorWarning:       fmt.Sprintf,
+		ColorInformation:   fmt.Sprintf,
+		ColorInternalError: fmt.Sprintf,
+		ColorUnknown:       fmt.Sprintf,
+	}
+	if ansi {
+		ret.ColorError = color.New(color.FgRed).SprintfFunc()
+		ret.ColorWarning = color.New(color.FgYellow).SprintfFunc()
+		ret.ColorInformation = color.New(color.FgGreen).SprintfFunc()
+		ret.ColorInternalError = color.New(color.FgHiRed).SprintfFunc()
+		ret.ColorUnknown = color.New(color.FgMagenta).SprintfFunc()
+	}
+	return ret
+}
+
+func (o *AnsiOutput) OnResult(p *panyl.Process) (cont bool) {
+	var out bytes.Buffer
+
+	// level
+	var levelColor AnsiOutputSprintfFunc
+	level := p.Metadata.StringValue(panyl.Metadata_Level)
+	switch level {
+	case panyl.MetadataLevel_DEBUG, panyl.MetadataLevel_INFO:
+		levelColor = o.ColorInformation
+	case panyl.MetadataLevel_WARNING:
+		levelColor = o.ColorWarning
+	case panyl.MetadataLevel_CRITICAL, panyl.MetadataLevel_FATAL:
+		levelColor = o.ColorError
+	default:
+		level = "unknown"
+		levelColor = o.ColorUnknown
+	}
+
+	// timestamp
+	if ts, ok := p.Metadata[panyl.Metadata_Timestamp]; ok {
+		out.WriteString(fmt.Sprintf("%s ", ts.(time.Time).Local().Format("2006-01-02 15:04:05.000")))
+	}
+
+	// application
+	if application := p.Metadata.StringValue(panyl.Metadata_Application); application != "" {
+		out.WriteString(fmt.Sprintf("| %s | ", application))
+	}
+
+	// level
+	if level != "" {
+		out.WriteString(fmt.Sprintf("[%s] ", level))
+	}
+
+	// format
+	if format := p.Metadata.StringValue(panyl.Metadata_Format); format != "" {
+		out.WriteString(fmt.Sprintf("(%s) ", format))
+	}
+
+	// category
+	if category := p.Metadata.StringValue(panyl.Metadata_Category); category != "" {
+		out.WriteString(fmt.Sprintf("{{%s}} ", category))
+	}
+
+	// message
+	if msg := p.Metadata.StringValue(panyl.Metadata_Message); msg != "" {
+		out.WriteString(msg)
+	} else if p.Line != "" {
+		out.WriteString(p.Line)
+	} else if len(p.Data) > 0 {
+		dt, err := json.Marshal(p.Data)
+		if err != nil {
+			fmt.Println(o.ColorInternalError("Error marshaling data to json: %s", err.Error()))
+			return
+		}
+		out.WriteString(fmt.Sprintf("| %s", string(dt)))
+	}
+
+	fmt.Println(levelColor(out.String()))
+
+	return true
+}
